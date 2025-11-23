a4e53e878436e6a2830b5e71653e677e02f947f5
haykal24 <moh.haikal02@gmail.com>
2025-11-23 15:22:02 +0700

    Add ShieldSeeder to DatabaseSeeder to create superadmin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SecurityDatabaseSeeder::class,
             SupportDatabaseSeeder::class,
+            ShieldSeeder::class,
         ]);
     }
 }
